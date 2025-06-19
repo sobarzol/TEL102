0e198d00896429be619877f5f0fa95e16aeefbe0
JavierIRamirezA <javier.ramireza@usm.cl>
2025-06-18 22:24:54 -0400

    desarrollo dia 4 xd

--- schedule.cpp
+++ schedule.cpp
@@ -2,6 +2,13 @@
 
 Schedule::Schedule() {}
 
+Schedule::Schedule(Dia aDay, int aHour, int aMinute) {
+    Day = aDay;
+    Hour = aHour;
+    Minute = aMinute;
+}
+
+
 bool Schedule::operator==(const Schedule& otra) const {
     return (static_cast<int>(Day) == static_cast<int>(otra.Day)) &&
            (Hour == otra.Hour) && (Minute == otra.Minute);
